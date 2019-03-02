08d8d6ea75390395fdd113ab1e3efe5cbf5f94e9
Philippe Boisseau <philippe.boisseau.1@ulaval.ca>
2019-03-02 12:06:23 -0500

    Ex2 fait

--- Env2/Ex2/src/Ex2.cpp
+++ Env2/Ex2/src/Ex2.cpp
@@ -7,9 +7,46 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int main()
+{
+	cout << "Entrer votre nom" << endl;
+
+	string nom;
+	char buffer[256];
+	cin.getline(buffer,256);
+	nom = buffer;
+
+	cout << "Entrer votre prenom" << endl;
+
+	string prenom;
+	cin.getline(buffer,256);
+	prenom = buffer;
+
+	cout << "Entrer votre date de naissance format (yyyy-mm-dd)" << endl;
+
+	string date;
+	cin.getline(buffer,256);
+	date = buffer;
+
+	stringstream chaine(date);
+	string annee;
+	string mois;
+	string jour;
+
+	chaine.getline(buffer, 256, '-');
+	annee = buffer;
+
+	chaine.getline(buffer, 256, '-');
+	mois = buffer;
+
+	chaine.getline(buffer, 256, '-');
+	jour = buffer;
+
+	cout << prenom.substr(0,2) << nom.substr(0,3) << jour << mois << endl;
+
 	return 0;
 }
